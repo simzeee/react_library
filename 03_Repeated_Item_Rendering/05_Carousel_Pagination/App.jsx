f4a80b7209f4addc9eba5384b8f7751661bad0f0
simzeee <jwsims816@gmail.com>
2024-10-25 16:25:03 -0400

    finish Carousel with pagination

--- 03_Repeated_Item_Rendering/05_Carousel_Pagination/App.jsx
+++ 03_Repeated_Item_Rendering/05_Carousel_Pagination/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import PostItem from "./PostItem";
 import posts from "./posts";
 
+const PAGE_SIZE = 9;
+
 const App = () => {
-  const postItems = posts.map((post, idx) => (
-    <PostItem key={idx} post={post} />
-  ));
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const postItems = posts
+    .slice(PAGE_SIZE * pageIndex, PAGE_SIZE * (pageIndex + 1))
+    .map((post, idx) => <PostItem key={idx} post={post} />);
+
+  const numPages = Math.ceil(posts.length / PAGE_SIZE);
+  const buttons = [];
+
+  for (let i = 0; i < numPages; i += 1) {
+    buttons.push(
+      <button
+        onClick={() => {
+          setPageIndex(i);
+        }}
+        key={i}
+        className="bg-gray-400 text-white text-small w-4 m-1"
+      >
+        {i + 1}
+      </button>
+    );
+  }
 
   return (
-    <div className="flex justify-center py-20">
+    <div className="flex flex-col items-center justify-center py-20">
       <div className="w-full max-w-4xl flex flex-wrap justify-center">
         {postItems}
       </div>
+      <div>{buttons}</div>
     </div>
   );
 };
