6c8aaa18b4c22b1a2d835f90d0ac077654416960
simzeee <jwsims816@gmail.com>
2024-10-26 21:25:42 -0400

    form completed

--- 04_inputs_forms/Sign_In_Form/App.jsx
+++ 04_inputs_forms/Sign_In_Form/App.jsx
@@ -6,9 +6,15 @@ const App = () => {
 
   return (
     <div className="flex justify-center p-20">
-      <div className="flex flex-col border border-neutral-300 shadow-lg rounded-lg">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log({ username: username, password: password });
+        }}
+        className="flex flex-col border border-neutral-300 shadow-lg rounded-lg"
+      >
         <div className="p-4 flex flex-col">
-          <div className="text-2xl text-neutral-600">Sign-In</div>
+          <div className="text-2xl text-neutral-600 mb-4">Sign-In</div>
           <input
             type="text"
             className="my-4 border border-neutral-300 p-2 text-neutral-600"
@@ -31,7 +37,7 @@ const App = () => {
         <button className="bg-purple-500 py-2 text-white rounded-b-lg">
           Submit
         </button>
-      </div>
+      </form>
     </div>
   );
 };
