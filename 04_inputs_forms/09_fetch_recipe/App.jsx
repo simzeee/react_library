fde5774c08540341a0f8b8ff535ef06a778858d2
simzeee <jwsims816@gmail.com>
2024-10-30 16:08:23 -0400

    finish fetch recipe

--- 04_inputs_forms/09_fetch_recipe/App.jsx
+++ 04_inputs_forms/09_fetch_recipe/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import RecipeCard from "./RecipeCard";
+
 const RECIPE = {
   name: "Pho",
   image_url: "https://static-task-assets.react-formula.com/239035.jpg",
 };
 
 const App = () => {
+  const [recipe, setRecipe] = useState(null);
+
+  const fetchRecipe = () => {
+    fetch(
+      "https://api.react-formula.com/learning-api/demos/using-fetch/recipes"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipe(data);
+      });
+  };
   return (
-    <div className="flex justify-center">
-      <RecipeCard recipe={RECIPE}></RecipeCard>
+    <div className="flex flex-col items-center justify-center">
+      {recipe !== null ? <RecipeCard recipe={recipe}></RecipeCard> : null}
+
+      <button
+        className="mt-2 rounded-full px-4 py-2 bg-blue-400 text-white"
+        onClick={fetchRecipe}
+      >
+        get random recipe
+      </button>
     </div>
   );
 };
