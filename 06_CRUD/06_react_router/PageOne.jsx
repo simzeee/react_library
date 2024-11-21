c07198e1fc28aa73249b65feb5e9606392187b69
simzeee <jwsims816@gmail.com>
2024-11-21 17:18:39 -0500

    finish react router phase 1

---
+++ 06_CRUD/06_react_router/PageOne.jsx
@@ -0,0 +1,7 @@
+const PageOne = () => {
+  return <div className="flex justify-center py-80 bg-green-300">
+    Page One
+  </div>;
+};
+
+export default PageOne;
---
+++ 06_CRUD/06_react_router/PageTwo.jsx
@@ -0,0 +1,7 @@
+const PageOne = () => {
+  return (
+    <div className="flex justify-center py-80 bg-purple-300">Page Two</div>
+  );
+};
+
+export default PageOne;
