6e6c7f13bb0aecab88edeb7619ec674eaaa15eb4
simzeee <jwsims816@gmail.com>
2024-11-22 15:31:05 -0500

    finish useRef project

---
+++ 08_other_react_hooks/UseRef/Example_3.jsx
@@ -0,0 +1,34 @@
+import { useState, useRef } from "react";
+
+const Example3 = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  return (
+    <div className="h-screen bg-emerald-100">
+      <h2 className="p-8 text-3xl font-bold text-green-700">Example 3</h2>
+      <button
+        onClick={() => {
+          if (isPlaying) {
+            videoRef.current.pause();
+            setIsPlaying(false);
+          } else {
+            videoRef.current.play();
+            setIsPlaying(true);
+          }
+        }}
+        className="px-8 py-2 mx-8 my-2 text-white rounded-full bg-emerald-600"
+      >
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <video ref={videoRef} width="480">
+        <source
+          src="https://static-task-assets.react-formula.com/kickflip.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </div>
+  );
+};
+
+export default Example3;
