8fc0ef7d6328145b9d140821969f218298c02cba
simzeee <jwsims816@gmail.com>
2024-11-25 14:38:45 -0500

    finish phase 0-initial setup

---
+++ 09_Responsive_Design/responsive-design-project/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="text-2xl font-ubuntu">
+      hello
+      <div className="font-roboto">
+        hello
+      </div>
+      <i className="fa-solid fa-face-smile"/>
+    </div>
+  );
+};
+
+export default App;
