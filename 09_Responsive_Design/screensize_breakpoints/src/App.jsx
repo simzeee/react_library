df4490a99bd249cb3ae0668c0bfe7e0f82b77c27
simzeee <jwsims816@gmail.com>
2024-11-25 13:45:06 -0500

    finish breakpoint exercise

--- 09_Responsive_Design/screensize_breakpoints/src/App.jsx
+++ 09_Responsive_Design/screensize_breakpoints/src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 
 const App = () => {
-  return <div className="">hello</div>;
+  return <div className="bg-green-300 sm:bg-yellow-500 md:bg-purple-400 md:font-bold text-blue-500 h-screen flex justify-center items-center text-4xl">hello</div>;
 };
 
 export default App;
